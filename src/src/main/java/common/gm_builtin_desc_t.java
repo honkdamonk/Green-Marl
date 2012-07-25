ff217ca6f0df7660bdbcfa6e9c9b49580744c970
honkdamonk <honkdamonk@googlemail.com>
2012-07-25 13:22:31 -0700

    fixed syntax errors

--- src/src/main/java/common/gm_builtin_desc_t.java
+++ src/src/main/java/common/gm_builtin_desc_t.java
@@ -6,6 +6,12 @@ package common;
 //-----------------------------------------------------
 public class gm_builtin_desc_t
 {
+	public gm_builtin_desc_t(String defString, gm_method_id_t methodId, String extraInfo) {
+		def_string = defString;
+		method_id = methodId;
+		extra_info = extraInfo;
+	}
+	
 	public String def_string; //source:type:name:return_type:num_args:arg_type0:arg_type1: ...
 	public gm_method_id_t method_id;
 	public String extra_info; //key:value,key:value,...

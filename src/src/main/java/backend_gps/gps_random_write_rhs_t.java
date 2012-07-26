911f49fd48c49307408c114a2c850049f2897e8d
honkdamonk <honkdamonk@googlemail.com>
2012-07-26 13:55:30 -0700

    some RefType-Stuff

--- src/src/main/java/backend_gps/gps_random_write_rhs_t.java
+++ src/src/main/java/backend_gps/gps_random_write_rhs_t.java
@@ -23,35 +23,35 @@ import common.gm_apply;
 //C++ TO JAVA CONVERTER NOTE: The following #define macro was replaced in-line:
 ///#define GM_BLTIN_FLAG_TRUE true
 
-
-
-public class gps_random_write_rhs_t extends gm_apply
-{
-	public gps_random_write_rhs_t(ast_sentblock _sb, gm_symtab_entry _sym, gm_gpslib l, gm_code_writer b)
-	{
-		this.sb = _sb;
-		this.sym = _sym;
-		this.lib = l;
-		this.Body = new gm_code_writer(b);
+public class gps_random_write_rhs_t extends gm_apply {
+	
+	private ast_sentblock sb;
+	private gm_symtab_entry sym;
+	private gm_gps_comm_unit U;
+	private gm_gps_beinfo INFO;
+	private gm_gpslib lib;
+	private gm_code_writer Body;
+	
+	public gps_random_write_rhs_t(ast_sentblock _sb, gm_symtab_entry _sym, gm_gpslib l, gm_code_writer b) {
+		sb = _sb;
+		sym = _sym;
+		lib = l;
+		Body = b;
 		set_for_expr(true);
 		U = new gm_gps_comm_unit(gm_gps_comm_t.GPS_COMM_RANDOM_WRITE, sb, sym);
 		INFO = (gm_gps_beinfo) GlobalMembersGm_main.FE.get_current_backend_info();
 	}
 
-	public final boolean apply(ast_expr e)
-	{
+	public final boolean apply(ast_expr e) {
 		if (!e.is_id() && !e.is_field())
 			return true;
 
 		gm_gps_communication_symbol_info SS;
 		gm_symtab_entry tg;
 
-		if (e.is_id())
-		{
+		if (e.is_id()) {
 			tg = e.get_id().getSymInfo();
-		}
-		else
-		{
+		} else {
 			tg = e.get_field().get_second().getSymInfo();
 			if (e.get_field().get_first().getSymInfo() == sym)
 				return true;
@@ -78,12 +78,5 @@ public class gps_random_write_rhs_t extends gm_apply
 		Body.pushln(";");
 		return true;
 	}
-
-	private ast_sentblock sb;
-	private gm_symtab_entry sym;
-	private gm_gps_comm_unit U;
-	private gm_gps_beinfo INFO;
-	private gm_gpslib lib;
-	private gm_code_writer Body;
 
 }

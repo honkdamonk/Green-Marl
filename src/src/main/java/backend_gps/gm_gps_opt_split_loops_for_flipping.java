086d21ebaf047d14878f723a6a280fe6778fd28c
honkdamonk <honkdamonk@googlemail.com>
2012-07-30 23:38:02 -0700

    more refactorings

--- src/src/main/java/backend_gps/gm_gps_opt_split_loops_for_flipping.java
+++ src/src/main/java/backend_gps/gm_gps_opt_split_loops_for_flipping.java
@@ -1,59 +1,544 @@
 package backend_gps;
 
+import frontend.GlobalMembersGm_rw_analysis;
+import frontend.GlobalMembersGm_rw_analysis_check2;
+import frontend.gm_range_type_t;
+import frontend.gm_rwinfo;
+import frontend.gm_symtab;
+import frontend.gm_symtab_entry;
 import inc.gm_compile_step;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+import ast.AST_NODE_TYPE;
 import ast.ast_foreach;
+import ast.ast_if;
+import ast.ast_node;
 import ast.ast_procdef;
+import ast.ast_sent;
+import ast.ast_sentblock;
+import ast.gm_rwinfo_list;
+import ast.gm_rwinfo_map;
 
 import common.GlobalMembersGm_flat_nested_sentblock;
+import common.GlobalMembersGm_new_sents_after_tc;
+import common.GlobalMembersGm_resolve_nc;
+import common.GlobalMembersGm_transform_helper;
 
-import frontend.GlobalMembersGm_rw_analysis;
+public class gm_gps_opt_split_loops_for_flipping extends gm_compile_step {
+	
+	private static String USED_BY_WHO = "gm_split_used_by_who";
+	private static final int USED_BY_OLDER = 1;
+	private static final int USED_BY_YOUNGER = 2;
 
-public class gm_gps_opt_split_loops_for_flipping extends gm_compile_step
-{
-	private gm_gps_opt_split_loops_for_flipping()
-	{
+	private gm_gps_opt_split_loops_for_flipping() {
 		set_description("(Pre-Flip) Splitting loops");
 	}
-	public void process(ast_procdef p)
-	{
-		//-------------------------------------
+
+	public void process(ast_procdef p) {
+		// -------------------------------------
 		// Find nested loops
-		//-------------------------------------
-		java.util.HashMap<ast_foreach, ast_foreach> MAP = new java.util.HashMap<ast_foreach, ast_foreach>();
-		java.util.HashSet<ast_foreach> SET = new java.util.HashSet<ast_foreach>();
+		// -------------------------------------
+		HashMap<ast_foreach, ast_foreach> MAP = new HashMap<ast_foreach, ast_foreach>();
+		HashSet<ast_foreach> SET = new HashSet<ast_foreach>();
 		GlobalMembersGm_gps_opt_find_nested_foreach_loops.gm_gps_find_double_nested_loops(p, MAP);
-    
-		//-------------------------------------
+
+		// -------------------------------------
 		// Find target inner loops
-		//-------------------------------------
-		GlobalMembersGm_gps_opt_split_loops_for_flipping.filter_target_loops(MAP, SET);
-    
-		//-------------------------------------
-		//  - Now split the loops
-		//-------------------------------------
-		for (ast_foreach fe : SET)
-		{
-			GlobalMembersGm_gps_opt_split_loops_for_flipping.split_the_loop(fe);
+		// -------------------------------------
+		filter_target_loops(MAP, SET);
+
+		// -------------------------------------
+		// - Now split the loops
+		// -------------------------------------
+		for (ast_foreach fe : SET) {
+			split_the_loop(fe);
 		}
-    
+
 		GlobalMembersGm_flat_nested_sentblock.gm_flat_nested_sentblock(p);
-    
+
 		// reconstruct_scope implied in flattening
-		//gm_reconstruct_scope(p);  
-    
-		//-------------------------------------
+		// gm_reconstruct_scope(p);
+
+		// -------------------------------------
 		// Re-do RW analysis
-		//-------------------------------------
+		// -------------------------------------
 		GlobalMembersGm_rw_analysis.gm_redo_rw_analysis(p.get_body());
-    
+
 	}
+
 	@Override
-	public gm_compile_step get_instance()
-	{
+	public gm_compile_step get_instance() {
 		return new gm_gps_opt_split_loops_for_flipping();
 	}
-	public static gm_compile_step get_factory()
-	{
+
+	public static gm_compile_step get_factory() {
 		return new gm_gps_opt_split_loops_for_flipping();
 	}
+	
+	private static void split_the_loop(ast_foreach in) {
+		ast_foreach out = null;
+
+		// ----------------------------------------------------------------
+		// find the current frame structure
+		// example>
+		// Foreach() <--- out
+		// {
+		// A1; A2;
+		// {
+		// B1; B2;
+		// If (...) {
+		// C1; C2;
+		// Froeach() { <--- in
+		// }
+		// C3; C4;
+		// }
+		// B3; B4;
+		// }
+		// A3; A4;
+		// }
+		//
+		// frame: C -> If -> B -> A
+		// older[A] = {A1; A2;}
+		// older[B] = {B1; B2;}
+		// older[C] = {C1; C2;}
+		// younger[A] = {A3; A4;}
+		// younger[B] = {B3; B4;}
+		// younger[C] = {C3; C4;}
+		// ----------------------------------------------------------------
+
+		LinkedList<ast_node> frame = new LinkedList<ast_node>();
+		HashMap<ast_sentblock, LinkedList<ast_sent>> older_siblings = new HashMap<ast_sentblock, LinkedList<ast_sent>>();
+		HashMap<ast_sentblock, LinkedList<ast_sent>> younger_siblings = new HashMap<ast_sentblock, LinkedList<ast_sent>>();
+
+		ast_node current = in;
+		boolean need_young = false;
+		boolean need_old = false;
+		while (true) {
+			ast_node node = current.get_parent();
+			assert node != null;
+			if (node.get_nodetype() == AST_NODE_TYPE.AST_IF) {
+				frame.addLast(node);
+				current = node;
+				continue;
+			} else if (node.get_nodetype() == AST_NODE_TYPE.AST_FOREACH) {
+				out = (ast_foreach) node;
+				break;
+			} else if (node.get_nodetype() == AST_NODE_TYPE.AST_SENTBLOCK) {
+				ast_sentblock sb = (ast_sentblock) node;
+				frame.addLast(sb);
+
+				// add elder and younger brothers
+				LinkedList<ast_sent> All = sb.get_sents();
+				LinkedList<ast_sent> OLD = new LinkedList<ast_sent>();
+				LinkedList<ast_sent> YOUNG = new LinkedList<ast_sent>();
+				boolean older = true;
+				for (ast_sent s : All) {
+					if (s == current) {
+						older = false;
+						continue;
+					}
+					if (older) {
+						OLD.addLast(s);
+					} else {
+						YOUNG.addLast(s);
+					}
+				}
+
+				All.clear();
+				All.addLast((ast_sent) current);
+
+				assert older == false;
+				older_siblings.put(sb, OLD);
+				younger_siblings.put(sb, YOUNG);
+				need_young = need_young || (YOUNG.size() > 0);
+				need_old = need_old || (OLD.size() > 0);
+				current = node;
+
+				continue; // go up one level
+			} else {
+				assert false;
+			}
+		}
+		assert out != null;
+
+		assert out.get_iter_type().is_iteration_on_all_graph();
+		GlobalMembersGm_transform_helper.gm_make_it_belong_to_sentblock(out);
+		assert out.get_parent().get_nodetype() == AST_NODE_TYPE.AST_SENTBLOCK;
+
+		// check if there are dependencies via scalar variable
+		// also mark each scalar symbols whether they are used by elder siblings
+		// or younger siblings
+		ensure_no_dependency_via_scala(frame, older_siblings, younger_siblings);
+
+		// ----------------------------------------------------------------
+		// reconstruct program
+		// ----------------------------------------------------------------
+		if (need_old) {
+			ast_node old = reconstruct_old_new(frame, older_siblings, true);
+			assert old != null;
+
+			ast_foreach old_loop = GlobalMembersGm_new_sents_after_tc.gm_new_foreach_after_tc(out.get_iterator().copy(false), out.get_source().copy(true),
+					(ast_sent) old, out.get_iter_type());
+			// replace iterator id
+			GlobalMembersGm_resolve_nc.gm_replace_symbol_entry(out.get_iterator().getSymInfo(), old_loop.get_iterator().getSymInfo(), old);
+
+			GlobalMembersGm_transform_helper.gm_add_sent_before(out, old_loop);
+
+		}
+
+		if (need_young) {
+			ast_node old = reconstruct_old_new(frame, younger_siblings, false);
+			assert old != null;
+			ast_foreach new_loop = GlobalMembersGm_new_sents_after_tc.gm_new_foreach_after_tc(out.get_iterator().copy(false), out.get_source().copy(true),
+					(ast_sent) old, out.get_iter_type());
+			// replace iterator id
+			GlobalMembersGm_resolve_nc.gm_replace_symbol_entry(out.get_iterator().getSymInfo(), new_loop.get_iterator().getSymInfo(), old);
+
+			GlobalMembersGm_transform_helper.gm_add_sent_after(out, new_loop);
+
+		}
+	}
+	
+	private static void ensure_no_dependency_via_scala(LinkedList<ast_node> frame, HashMap<ast_sentblock, LinkedList<ast_sent>> elder,
+			HashMap<ast_sentblock, LinkedList<ast_sent>> younger) {
+		// create prev/next
+		// create set of scalar symbols used in prev
+		HashSet<gm_symtab_entry> PREVS = new HashSet<gm_symtab_entry>(); 
+		// symbols used in next
+		HashSet<gm_symtab_entry> NEXTS = new HashSet<gm_symtab_entry>();
+		// every scalar symbols
+		HashSet<gm_symtab_entry> ALL = new HashSet<gm_symtab_entry>();
+		
+		for (ast_node node : frame) {
+			if (node.get_nodetype() == AST_NODE_TYPE.AST_IF) {
+				// continue
+			} else if (node.get_nodetype() == AST_NODE_TYPE.AST_SENTBLOCK) {
+				ast_sentblock sb = (ast_sentblock) node;
+				LinkedList<ast_sent> OLD = elder.get(sb);
+				LinkedList<ast_sent> NEW = younger.get(sb);
+				for (ast_sent sent : OLD) {
+					add_scalar_rw(sent, PREVS);
+				}
+
+				for (ast_sent sent : NEW) {
+					add_scalar_rw(sent, NEXTS);
+				}
+
+				HashSet<gm_symtab_entry> E = sb.get_symtab_var().get_entries();
+				for (gm_symtab_entry e : E) {
+					ALL.add(e);
+				}
+			} else {
+				assert false;
+			}
+		}
+
+		// check if any entry is both used PREV and NEXT
+		for (gm_symtab_entry e : ALL) {
+			if ((PREVS.contains(e)) && (NEXTS.contains(e))) {
+				assert false;
+				// [todo] replace these symbols with temporary node_prop
+				// re-do rw-analysis afterward.
+			} else if (PREVS.contains(e))
+				e.add_info_int(USED_BY_WHO, USED_BY_OLDER);
+			else if (NEXTS.contains(e))
+				e.add_info_int(USED_BY_WHO, USED_BY_YOUNGER);
+		}
+	}
+
+	
+
+	private static void add_scalar_rw(ast_sent s, HashSet<gm_symtab_entry> TARGET) {
+		gm_rwinfo_map W = GlobalMembersGm_rw_analysis_check2.gm_get_write_set(s);
+		gm_rwinfo_map R = GlobalMembersGm_rw_analysis_check2.gm_get_write_set(s);
+		for (gm_symtab_entry e : W.keySet()) {
+			if (!e.getType().is_property()) {
+				TARGET.add(e);
+			}
+		}
+		for (gm_symtab_entry e : R.keySet()) {
+			if (!e.getType().is_property()) {
+				TARGET.add(e);
+			}
+		}
+	}
+	
+	private static void reconstruct_old_new_main(ast_node n, HashMap<ast_sentblock, LinkedList<ast_sent>> siblings, boolean is_old, ast_node last) {
+
+		if (n.get_nodetype() == AST_NODE_TYPE.AST_IF) {
+			if (last == null) // can ignore this if loop
+				return;
+
+			ast_if iff = (ast_if) (n);
+			ast_if new_if = ast_if.new_if(iff.get_cond().copy(true), (ast_sent) last, null);
+			new_if.set_line(iff.get_line());
+			new_if.set_col(iff.get_col());
+			last = new_if;
+
+		} else if ((n).get_nodetype() == AST_NODE_TYPE.AST_SENTBLOCK) {
+			ast_sentblock sb_org = (ast_sentblock) (n);
+			ast_sentblock sb = ast_sentblock.new_sentblock();
+			LinkedList<ast_sent> SIB = siblings.get(sb_org);
+			sb.set_line(sb_org.get_line());
+			sb.set_col(sb_org.get_col());
+
+			if (is_old) {
+				for (ast_sent sent : SIB) {
+					sb.add_sent(sent);
+				}
+				if (last != null)
+					sb.add_sent((ast_sent) last);
+			} else {
+				if (last != null)
+					sb.add_sent((ast_sent) last);
+
+				for (ast_sent sent : SIB) {
+					sb.add_sent(sent);
+				}
+			}
+
+			last = sb;
+
+			// move scalar symbols
+			gm_symtab old_tab = sb_org.get_symtab_var();
+			gm_symtab new_tab = sb.get_symtab_var();
+			HashSet<gm_symtab_entry> entries = old_tab.get_entries();
+			HashSet<gm_symtab_entry> T = new HashSet<gm_symtab_entry>();
+			for (gm_symtab_entry e : entries) {
+				int used_by = e.find_info_int(USED_BY_WHO);
+				if ((used_by == USED_BY_OLDER) && (is_old)) {
+					T.add(e);
+				} else if ((used_by == USED_BY_YOUNGER) && (!is_old)) {
+					T.add(e);
+				}
+			}
+			for (gm_symtab_entry e : T) {
+				old_tab.remove_entry_in_the_tab(e);
+				new_tab.add_symbol(e);
+			}
+		}
+	}
+	
+	private static gm_rwinfo_map gm_get_reduce_set(ast_sent S) {
+		assert S != null;
+		return GlobalMembersGm_rw_analysis.get_rwinfo_sets(S).reduce_set;
+	}
+	
+	// --------------------------------------------
+	// example>
+	// FE(n:)
+	// {
+	// A;
+	// {
+	// B;
+	// If()
+	// {
+	// C;
+	// Foreach(t:) {
+	// }
+	// D;
+	// }
+	// E;
+	// }
+	// F;
+	// }
+	// =====>
+	// FE(n:)
+	// {
+	// A;
+	// {
+	// B;
+	// If()
+	// {
+	// C;
+	// } } }
+	//
+	// FE(n1:)
+	// { { If() {
+	// Foreach(t:) {
+	// }
+	// }
+	// } }
+	//
+	// FE(n2:)
+	// { { If()
+	// {
+	// D;
+	// }
+	// E;
+	// }
+	// F;
+	// }
+	// ------------------------------------------
+	private static ast_node reconstruct_old_new(LinkedList<ast_node> frame, HashMap<ast_sentblock, LinkedList<ast_sent>> siblings, boolean is_old) {
+		ast_node last = null;
+
+		// reconstruct hierarchy
+		// inmost --> outmost
+		for (ast_node node : frame) {
+			reconstruct_old_new_main(node, siblings, is_old, last);
+		}
+
+		return last;
+	}
+	
+	// ----------------------------------------------------
+	// N_P<Int>(G) _tmp_S;
+	// Foreach (n: G.Nodes) {
+	// n._tmp_S = 0;
+	// Foreach( t: n.InNbrs) {
+	// n._tmp_S += t.A;
+	// }
+	// n.B = n._tmp_S * alpha + beta;
+	// }
+	// ===>
+	// Foreach (n: G.Nodes) {
+	// n._tmp_S = 0;
+	// }
+	// Foreach (n: G.Nodes) {
+	// Foreach( t: n.InNbrs) {
+	// n._tmp_S += t.A;
+	// }
+	// }
+	// Foreach (n: G.Nodes) {
+	// n.B = n._tmp_S * alpha + beta;
+	// }
+	// ----------------------------------------------------
+	//
+
+	// ---------------------------------------
+	//
+	// Foreach(n.) {
+	// .... (A)
+	//
+	// <if> { // no variables defined in between
+	// Foreach(t) {
+	// n.X = ...
+	// }
+	// }
+	//
+	// .... (B)
+	// }
+	// ---------------------------------------
+	private static void filter_target_loops(HashMap<ast_foreach, ast_foreach> SRC, HashSet<ast_foreach> SET) {
+		for (ast_foreach in : SRC.keySet()) {
+			ast_foreach out = SRC.get(in);
+			if (out == null)
+				continue;
+
+			gm_symtab_entry out_iter = out.get_iterator().getSymInfo();
+
+			boolean is_target = false;
+			// check if inner loop requires flipping
+			gm_rwinfo_map WMAP = GlobalMembersGm_rw_analysis_check2.gm_get_write_set(in.get_body());
+			for (gm_symtab_entry e : WMAP.keySet()) {
+				gm_rwinfo_list LIST = WMAP.get(e);
+				boolean is_field = e.getType().is_property();
+				for (gm_rwinfo info : LIST) {
+					if (is_field && (info.driver == null) && (info.access_range == gm_range_type_t.GM_RANGE_RANDOM)) {
+						is_target = true;
+						continue;
+					} else if (info.driver == null) {
+						continue;
+					}
+
+					if (info.driver != out_iter) {
+						is_target = false;
+						break;
+					}
+					if (info.driver == out_iter) {
+						is_target = true;
+						continue;
+					}
+				}
+			}
+
+			gm_rwinfo_map DMAP = gm_get_reduce_set(in.get_body());
+			for (gm_symtab_entry e : DMAP.keySet()) {
+				gm_rwinfo_list LIST = DMAP.get(e);
+				for (gm_rwinfo info : LIST) {
+					if ((info.driver == null) && (info.access_range == gm_range_type_t.GM_RANGE_RANDOM)) {
+						continue;
+					} else if (info.driver == null)
+						continue;
+
+					if (info.driver != out_iter) {
+						is_target = false;
+						break;
+					}
+					if (info.driver == out_iter) {
+						is_target = true;
+						continue;
+					}
+				}
+			}
+
+			// printf(" loop for %s -> %c \n",
+			// in->get_iterator()->get_genname(), is_target?'Y':'N');
+			if (!is_target)
+				continue;
+
+			boolean meet_if = false;
+			ast_node current = in;
+			// printf("current = %p %s\n", current,
+			// gm_get_nodetype_string(in->get_nodetype()));
+			// move up until meet the outer loop
+			while (true) {
+				ast_node parent = current.get_parent();
+				assert parent != null;
+				// printf(" parent = %p %s\n", parent,
+				// gm_get_nodetype_string(parent->get_nodetype()));
+				if (parent.get_nodetype() == AST_NODE_TYPE.AST_IF) {
+					if (meet_if) {
+						is_target = false;
+						break;
+					} else if (((ast_if) parent).get_else() == null) {
+						meet_if = true;
+					} else {
+						is_target = false;
+						break;
+					}
+				} else if (parent.get_nodetype() == AST_NODE_TYPE.AST_SENTBLOCK) {
+					// todo: this is a little bit to restrictive.
+					// we can relax this by changing more scalars into node
+					// properties.
+					ast_sentblock sb = (ast_sentblock) parent;
+
+					if (sb.get_symtab_field().get_num_symbols() > 0) {
+						is_target = false;
+						break;
+					}
+					/*
+					 * if ((sb->get_symtab_field()->get_num_symbols() > 0) ||
+					 * (sb->get_symtab_var()->get_num_symbols() > 0)) {
+					 */
+				} else if (parent.get_nodetype() == AST_NODE_TYPE.AST_FOREACH) {
+					ast_foreach fe = (ast_foreach) parent;
+					if (fe == out)
+						break;
+					else {
+						is_target = false;
+						break;
+					}
+				} else {
+					// printf("parent  = %s!!!\n",
+					// gm_get_nodetype_string(parent->get_nodetype()));
+					is_target = false;
+					break;
+				}
+				current = parent;
+			}
+
+			// printf(" loop for %s -> %c \n",
+			// in->get_iterator()->get_genname(), is_target?'Y':'N');
+			if (!is_target)
+				continue;
+			SET.add(in);
+		}
+	}
+
+
 }
--- src/src/main/java/frontend/GlobalMembersGm_rw_analysis_check2.java
+++ src/src/main/java/frontend/GlobalMembersGm_rw_analysis_check2.java
@@ -1,247 +1,11 @@
 package frontend;
 
-import static common.GM_ERRORS_AND_WARNINGS.GM_ERROR_MUTATE_MUTATE_CONFLICT;
-import static common.GM_ERRORS_AND_WARNINGS.GM_ERROR_READ_MUTATE_CONFLICT;
-import static common.GM_ERRORS_AND_WARNINGS.GM_ERROR_READ_REDUCE_CONFLICT;
-import static common.GM_ERRORS_AND_WARNINGS.GM_ERROR_READ_WRITE_CONFLICT;
-import static common.GM_ERRORS_AND_WARNINGS.GM_ERROR_WRITE_MUTATE_CONFLICT;
-import static common.GM_ERRORS_AND_WARNINGS.GM_ERROR_WRITE_REDUCE_CONFLICT;
-import static common.GM_ERRORS_AND_WARNINGS.GM_ERROR_WRITE_WRITE_CONFLICT;
-import static frontend.gm_conflict_t.MM_CONFLICT;
-import static frontend.gm_conflict_t.RD_CONFLICT;
-import static frontend.gm_conflict_t.RM_CONFLICT;
-import static frontend.gm_conflict_t.RW_CONFLICT;
-import static frontend.gm_conflict_t.WM_CONFLICT;
-import static frontend.gm_conflict_t.WW_CONFLICT;
-import static frontend.gm_range_type_t.GM_RANGE_LEVEL;
-import static frontend.gm_range_type_t.GM_RANGE_LEVEL_DOWN;
-import static frontend.gm_range_type_t.GM_RANGE_LEVEL_UP;
-import inc.GMTYPE_T;
-import inc.GM_REDUCE_T;
-
-import java.util.LinkedList;
-
-import tangible.RefObject;
 import ast.AST_NODE_TYPE;
 import ast.ast_sent;
-import ast.gm_rwinfo_list;
 import ast.gm_rwinfo_map;
 
-import common.GM_ERRORS_AND_WARNINGS;
-import common.GlobalMembersGm_error;
-
 public class GlobalMembersGm_rw_analysis_check2 {
-
-	private static boolean is_reported(LinkedList<conf_info_t> errors, gm_symtab_entry t, gm_symtab_entry b, gm_conflict_t y) {
-		for (conf_info_t db : errors) {
-			if ((db.sym1 == t) && (db.sym2 == b) && (db.conflict_type == y))
-				return true;
-		}
-		return false;
-	}
-
-	private static void add_report(LinkedList<conf_info_t> errors, gm_symtab_entry t, gm_symtab_entry b, gm_conflict_t conf_type) {
-		conf_info_t T = new conf_info_t();
-		T.sym1 = t;
-		T.sym2 = b;
-		T.conflict_type = conf_type;
-		errors.addLast(T);
-	}
-
-	// ---------------------------------------------------------
-	// [Read-Write conflict]
-	// Foreach(t: G.Nodes) {
-	// Forach(u: t.Nbrs) {
-	// t.C += u.A; // read A (random)
-	// }
-	// t.A = t.B + 3; // write A (linear) --> Error
-	// }
-	//
-	// Foreach(t: G.Nodes) {
-	// Foreach(u: t.Nbrs)
-	// u.A <= t.A; // defer
-	// t.A = t.B + 3; // write (okay)
-	// }
-	//
-	// BFS(t:G.nodes) {
-	// t.B = Sum(u:G.UpNbrs) u.A; // read A (LEV +1)
-	// t.A = t.B + 3; // write A (LEV) --> Okay
-	// }
-	// ---------------------------------------------------------
-	// [Write-Write conflict]
-	// Foreach(t: G.Nodes) {
-	// Forach(u: t.Nbrs) {
-	// u.A += t.A + u.B; // write A (random) [-->Error]
-	// }
-	// }
-	//
-	// BFS(t:G.nodes) {
-	// t.B = Sum(u:G.UpNbrs) u.A; // read A (LEV +1)
-	// t.A = t.B + 3; // write A (LEV) --> Okay
-	// }
-	// ----------------------------------------------------------
-
-	// ----------------------------------------------------------
-	// Examine two lists (for the same symbol)
-	// returns true if conflict
-	// - If they have different level, they do not conflict
-	// - (R-W, W-W) If they have the same driver, they do not conflict
-	// - (R-D) => If D is 'deferring write', no conflict
-	// ----------------------------------------------------------
-	public static int check_leveled_access(gm_rwinfo e1) {
-		// -----------------------
-		// -1: no level
-		// 0: lev-1
-		// 1: lev
-		// 2: lev+1
-		// -----------------------
-		int lev = -1;
-		gm_range_type_t a_range;
-		if (e1.driver == null) {
-			a_range = e1.access_range;
-		} else {
-			GMTYPE_T t = e1.driver.getType().get_typeid();
-			a_range = GlobalMembersGm_rw_analysis.gm_get_range_from_itertype(t);
-		}
-
-		if (a_range == GM_RANGE_LEVEL)
-			lev = 1;
-		else if (a_range == GM_RANGE_LEVEL_UP)
-			lev = 2;
-		else if (a_range == GM_RANGE_LEVEL_DOWN)
-			lev = 0;
-
-		return lev;
-	}
-
-	public static boolean check_if_conflict(gm_rwinfo_list l1, gm_rwinfo_list l2, RefObject<gm_rwinfo> e1_ref, RefObject<gm_rwinfo> e2_ref,
-			gm_conflict_t conf_type) {
-		java.util.Iterator<gm_rwinfo> i1;
-		java.util.Iterator<gm_rwinfo> i2;
-		gm_rwinfo e1 = null;
-		gm_rwinfo e2 = null;
-		for (i1 = l1.iterator(); i1.hasNext();) {
-			e1 = i1.next();
-			for (i2 = l2.iterator(); i2.hasNext();) {
-				e2 = i2.next();
-				// check if different level
-				int lev1 = GlobalMembersGm_rw_analysis_check2.check_leveled_access(e1);
-				int lev2 = GlobalMembersGm_rw_analysis_check2.check_leveled_access(e2);
-				if ((lev1 >= 0) && (lev2 >= 0) && (lev1 != lev2)) // different
-																	// level
-					continue;
-
-				if ((conf_type == RW_CONFLICT) || (conf_type == WW_CONFLICT) || (conf_type == RM_CONFLICT) || (conf_type == WM_CONFLICT)) {
-					if ((e1.driver != null) && (e1.driver == e2.driver))
-						continue;
-				}
-				if (conf_type == RD_CONFLICT) {
-					if (e2.reduce_op == GM_REDUCE_T.GMREDUCE_DEFER)
-						continue;
-					System.out.printf("%d lev1 = %d, %d lev2 = %d\n", e1.access_range, lev1, e2.access_range, lev2);
-					assert false;
-				}
-				if (conf_type == MM_CONFLICT) {
-					if (e1.mutate_direction == e2.mutate_direction)
-						continue;
-				}
-
-				// printf("lev1 = %d, lev2 = %d\n", lev1, lev2);
-				e1_ref.argvalue = e1;
-				e2_ref.argvalue = e2;
-				return true; // found conflict!
-			}
-		}
-		e1_ref.argvalue = e1;
-		e2_ref.argvalue = e2;
-		return false; // no conflict
-	}
-
-	// ------------------------------------------------------------
-	// [todo] Handling Scalar conflict (need scope info as well)
-	// check if two sets may conflict with each other
-	// return is_okay.
-	// ------------------------------------------------------------
-	public static boolean check_rw_conf_error(gm_rwinfo_map S1, gm_rwinfo_map S2, gm_conflict_t conf_type, LinkedList<conf_info_t> Report) {
-		boolean is_okay = true;
-		boolean is_warning;
-		GM_ERRORS_AND_WARNINGS error_code;
-		switch (conf_type) {
-		case RW_CONFLICT:
-			error_code = GM_ERROR_READ_WRITE_CONFLICT;
-			is_warning = true;
-			break;
-		case WW_CONFLICT:
-			error_code = GM_ERROR_WRITE_WRITE_CONFLICT;
-			is_warning = true;
-			break;
-		case RD_CONFLICT:
-			error_code = GM_ERROR_READ_REDUCE_CONFLICT;
-			is_warning = false;
-			break;
-		case WD_CONFLICT:
-			error_code = GM_ERROR_WRITE_REDUCE_CONFLICT;
-			is_warning = false;
-			break;
-		case RM_CONFLICT:
-			error_code = GM_ERROR_READ_MUTATE_CONFLICT;
-			is_warning = true;
-			break;
-		case WM_CONFLICT:
-			error_code = GM_ERROR_WRITE_MUTATE_CONFLICT;
-			is_warning = false;
-			break;
-		case MM_CONFLICT:
-			error_code = GM_ERROR_MUTATE_MUTATE_CONFLICT;
-			is_warning = true;
-			break;
-		default:
-			assert false;
-			throw new AssertionError();
-		}
-
-		for (gm_symtab_entry sym1 : S1.keySet()) {
-			gm_rwinfo_list list1 = S1.get(sym1);
-			gm_rwinfo e1 = null;
-
-			// Damn o.O if (!sym1->getType()->is_property()) continue; // todo
-			// 'scalar' check
-
-			for (gm_symtab_entry sym2 : S2.keySet()) {
-				gm_rwinfo_list list2 = S2.get(sym2);
-				gm_rwinfo e2 = null;
-
-				// find same symbol
-				if (sym1 != sym2)
-					continue;
-
-				// find if they conflict
-				RefObject<gm_rwinfo> e1_ref = new RefObject<gm_rwinfo>(e1);
-				RefObject<gm_rwinfo> e2_ref = new RefObject<gm_rwinfo>(e2);
-				boolean is_error_or_warn = GlobalMembersGm_rw_analysis_check2.check_if_conflict(list1, list2, e1_ref, e2_ref, conf_type);
-				e1 = e1_ref.argvalue;
-				e2 = e2_ref.argvalue;
-				if (!is_warning)
-					is_okay = is_okay && !is_error_or_warn;
-
-				// find if they report
-				if (is_error_or_warn) {
-					if (!GlobalMembersGm_rw_analysis_check2.is_reported(Report, sym1, sym2, conf_type)) {
-						GlobalMembersGm_rw_analysis_check2.add_report(Report, sym1, sym2, conf_type);
-						GlobalMembersGm_error.gm_conf_error(error_code, sym1, e1.location, e2.location, is_warning);
-					}
-				}
-			}
-		}
-		return is_okay;
-	}
-
-	public static boolean gm_check_parall_conflict_error(ast_sent b) {
-		gm_check_conf_t T = new gm_check_conf_t();
-		b.traverse_post(T); // post apply
-		return T.is_okay;
-	}
-
+		
 	// ==================================================================
 	// For depenendcy detection
 	// ==================================================================
@@ -272,14 +36,6 @@ public class GlobalMembersGm_rw_analysis_check2 {
 		return false;
 	}
 
-
-
-	
-	public static gm_rwinfo_map gm_get_reduce_set(ast_sent S) {
-		assert S != null;
-		return GlobalMembersGm_rw_analysis.get_rwinfo_sets(S).reduce_set;
-	}
-
 	public static gm_rwinfo_map gm_get_write_set(ast_sent S) {
 		assert S != null;
 		return GlobalMembersGm_rw_analysis.get_rwinfo_sets(S).write_set;
@@ -295,26 +51,26 @@ public class GlobalMembersGm_rw_analysis_check2 {
 		gm_rwinfo_map Q_M = Q_SET.mutate_set;
 
 		// true dependency
-		if (GlobalMembersGm_rw_analysis_check2.gm_does_intersect(P_W, Q_R, false))
+		if (gm_does_intersect(P_W, Q_R, false))
 			return true;
 		// anti dependency
-		if (GlobalMembersGm_rw_analysis_check2.gm_does_intersect(P_R, Q_W, false))
+		if (gm_does_intersect(P_R, Q_W, false))
 			return true;
 		// output dep
-		if (GlobalMembersGm_rw_analysis_check2.gm_does_intersect(P_W, Q_W, false))
+		if (gm_does_intersect(P_W, Q_W, false))
 			return true;
 		// write & muate => dependency
-		if (GlobalMembersGm_rw_analysis_check2.gm_does_intersect(P_W, Q_M, false))
+		if (gm_does_intersect(P_W, Q_M, false))
 			return true;
-		if (GlobalMembersGm_rw_analysis_check2.gm_does_intersect(P_M, Q_W, false))
+		if (gm_does_intersect(P_M, Q_W, false))
 			return true;
 		// read & mutate => dependency
-		if (GlobalMembersGm_rw_analysis_check2.gm_does_intersect(P_R, Q_M, false))
+		if (gm_does_intersect(P_R, Q_M, false))
 			return true;
-		if (GlobalMembersGm_rw_analysis_check2.gm_does_intersect(P_M, Q_R, false))
+		if (gm_does_intersect(P_M, Q_R, false))
 			return true;
 		// mutate & mutate => it depends on mutate_direction
-		if (GlobalMembersGm_rw_analysis_check2.gm_does_intersect(P_M, Q_M, true))
+		if (gm_does_intersect(P_M, Q_M, true))
 			return true;
 
 		return false;

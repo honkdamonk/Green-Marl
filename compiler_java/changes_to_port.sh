46f88d31c89062f20817a988ee61b8e2442ab0e3
Jan van der Lugt <janlugt@gmail.com>
2012-09-11 11:41:48 -0700

    updated todo

--- compiler_java/changes_to_port.sh
+++ compiler_java/changes_to_port.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-git diff -w 9a24a7652f375708adcfdad7ff769b1a47c801b7 master -- src/
+#git diff -w 9a24a7652f375708adcfdad7ff769b1a47c801b7 master -- src/
+git diff -w 06f6131681714bf342734f0db9bc12c6e3cc4cb4 master -- src/

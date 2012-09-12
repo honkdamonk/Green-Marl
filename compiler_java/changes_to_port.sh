20f0822a7bad0bf5c1c52b5076f3fc41f388790e
Jan van der Lugt <janlugt@gmail.com>
2012-09-12 00:09:44 -0700

    todo

--- compiler_java/changes_to_port.sh
+++ compiler_java/changes_to_port.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ..
 #git diff -w 06f6131681714bf342734f0db9bc12c6e3cc4cb4 master -- src/
-git diff -w 705066a37aee984fe22244b5daac1154183da75c master -- src/
+#git diff -w 705066a37aee984fe22244b5daac1154183da75c master -- src/
+git diff -w d6d5d4f1f2d0ef8a7ffa9053643e68a95421edb1 master -- src/

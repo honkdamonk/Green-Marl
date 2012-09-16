fa1151440e1bbb0bba98878aae8f501285ce603f
Jan van der Lugt <janlugt@gmail.com>
2012-09-16 01:04:24 -0700

    removed nop_enum_for_frontend

--- compiler_java/src/main/java/frontend/ast_temp_marker.java
+++ compiler_java/src/main/java/frontend/ast_temp_marker.java
@@ -6,6 +6,6 @@ import ast.ast_nop;
 public class ast_temp_marker extends ast_nop {
 
 	public ast_temp_marker() {
-		set_subtype(nop_enum_cpp.forValue(nop_enum_for_frontend.NOP_DUMMY_MARKER.getValue())); // FIXME
+		set_subtype(nop_enum_cpp.NOP_REDUCE_SCALAR);
 	}
 }
--- compiler_java/src/main/java/inc/nop_enum_cpp.java
+++ compiler_java/src/main/java/inc/nop_enum_cpp.java
@@ -4,11 +4,5 @@ package inc;
 // (NOPS) for CPP/CPP_LIB
 //---------------------------------------------------
 public enum nop_enum_cpp {
-
 	NOP_REDUCE_SCALAR;
-
-	public static nop_enum_cpp forValue(int value) {
-		return values()[value];
-	}
-
 }

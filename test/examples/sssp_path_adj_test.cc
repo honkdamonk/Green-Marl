ddae7d9a6ef4f88218e3e91e08da47b6dc3a2e85
Martin Sevenich <sevenich@in.tum.de>
2013-02-07 12:38:06 -0800

    finished unit test for sssp_path_adj example

--- test/examples/sssp_path_adj_test.cc
+++ test/examples/sssp_path_adj_test.cc
@@ -1,4 +1,5 @@
 #include "sssp_path_adj.h"
+#include <limits.h>
 
 int main(int argc, char** argv) {
 
@@ -11,44 +12,47 @@ int main(int argc, char** argv) {
     edge_schema.push_back(GMTYPE_DOUBLE);
 
     gm_graph G;
-    G.load_edge_list("graphs/test.txt", node_schema, edge_schema, node_props, edge_props);
+    G.load_edge_list("graphs/sssp_path_adj_testgraph.txt", node_schema, edge_schema, node_props, edge_props);
 
     double* edgeCosts = (double*) edge_props[0];
+    double* actual_dist;
+    node_t* actual_prev;
+    edge_t* actual_prev_e;
 
-    double* actual_dist = new double[G.num_nodes()];
-    node_t* actual_prev = new node_t[G.num_nodes()];
-    edge_t* actual_prev_e = new edge_t[G.num_nodes()];
     node_t root = 0;
-    node_t end = 4;
-
-    sssp_path(G, actual_dist, edgeCosts, root, end, actual_prev, actual_prev_e);
 
     double* target_dist = (double*) node_props[0];
     node_t* target_prev = (node_t*) node_props[1];
     edge_t* target_prev_e = (edge_t*) node_props[2];
 
-    const double tolerance = 0.000005;
+    for (node_t end = 0; end < G.num_nodes(); end++) {
 
-    for (node_t i = 0; i < G.num_nodes(); i++) {
-        double actual_d = actual_dist[i];
-        double target_d = target_dist[i];
+        actual_dist = new double[G.num_nodes()];
+        actual_prev = new node_t[G.num_nodes()];
+        actual_prev_e = new edge_t[G.num_nodes()];
 
-        double diff = std::abs(actual_d - target_d);
-        printf("%d: %lf %lf\n", i, actual_d, target_d);
-        assert(diff < tolerance || (actual_d == gm_get_max<int>() && target_d == -1));
+        sssp_path(G, actual_dist, edgeCosts, root, end, actual_prev, actual_prev_e);
 
-        node_t actual_p = actual_prev[i];
-        node_t target_p = target_prev[i];
-        printf("%d: %d %d\n", i, actual_p, target_p);
-        assert(actual_p == target_p);
-    }
+        const double tolerance = 0.000005;
 
-    for(node_t i = 0; i < G.num_nodes(); i++) {
-        edge_t actual_e = actual_prev_e[i];
-        edge_t target_e = target_prev_e[i];
-        if(actual_e != target_e) {
-            printf("%d: %d %d\n", i, actual_e, target_e);
-            assert(actual_e != target_e);
+        node_t x = end;
+        while (x != root) {
+            double actual_d = actual_dist[x];
+            double target_d = target_dist[x];
+            double diff = std::abs(actual_d - target_d);
+            assert(diff < tolerance || (actual_d == gm_get_max<double>() && target_d == -1));
+
+            node_t actual_p = actual_prev[x];
+            node_t target_p = target_prev[x];
+            assert(actual_p == target_p);
+            //the test could crash here though its correct if there is another shortest path
+            //but honestyl the likelyhood is very very low ;)
+
+            edge_t actual_e = actual_prev_e[x];
+            edge_t target_e = target_prev_e[x];
+            assert(actual_e == target_e);
+
+            x = target_prev[x];
         }
     }
 }

83b4138d3c30768fbc7bac5ccacfefb9ef224aad
Martin Sevenich <sevenich@in.tum.de>
2013-02-15 03:02:28 -0800

    added gm_seq using std::vector

--- apps/output_cpp/gm_graph/inc/gm_seq.h
+++ apps/output_cpp/gm_graph/inc/gm_seq.h
@@ -9,7 +9,7 @@
 template<typename T>
 class gm_seq
 {
-  public:
+public:
     gm_seq() {
         init(gm_rt_get_num_threads());
     }
@@ -113,7 +113,7 @@ class gm_seq
         return NULL;
     }
 
-  private:
+private:
 
     typename std::list<T> Q;
     typename std::list<T>* local_Q_front;
@@ -136,5 +136,136 @@ class gm_seq
 
 typedef gm_seq<node_t> gm_node_seq;
 typedef gm_seq<edge_t> gm_edge_seq;
+
+template<typename T>
+class gm_seq_vec
+{
+private:
+
+    typename std::vector<T> data;
+    typename std::vector<T>* local_back;
+    typename std::vector<T>* local_front;
+
+    int max_thread;
+    static const int THRESHOLD = 1024;
+
+    void init(int _max_thread) {
+        if (_max_thread > THRESHOLD) {
+            printf("error, too many # threads:%d\n", _max_thread);
+            abort();
+            max_thread = THRESHOLD;
+        }
+        max_thread = _max_thread;
+        local_back = new std::vector<T>[max_thread];
+        local_front = new std::vector<T>[max_thread];
+    }
+
+public:
+    gm_seq_vec() {
+        init(gm_rt_get_num_threads());
+    }
+    gm_seq_vec(int _max_thread) {
+        init(_max_thread);
+    }
+
+    virtual ~gm_seq_vec() {
+        delete[] local_front;
+        delete[] local_back;
+    }
+
+    //------------------------------------------------------------
+    // API
+    //   push_back/front, pop_back/front, clear, get_size
+    //   push has separate parallel interface
+    //------------------------------------------------------------
+    void push_back(T e) {
+        data.push_back(e);
+    }
+
+    void push_front(T e) {
+        data.push_front(e);
+    }
+
+    void pop_back() {
+        data.pop_back();
+    }
+
+    void pop_front() {
+        data.pop_front();
+    }
+
+    void clear() {
+        data.clear();
+    }
+
+    int get_size() {
+        return data.size();
+    }
+
+    // for parallel execution
+    void push_back_par(T e, int tid) {
+        local_back[tid].push_back(e);
+    }
+
+    void push_front_par(T e, int tid) {
+        local_front[tid].push_front(e);
+    }
+
+    // parallel pop is prohibited
+
+    //-------------------------------------------
+    // called when parallel addition is finished
+    //-------------------------------------------
+    void merge() {
+        for (int i = 0; i < max_thread; i++) {
+            if (local_front[i].size() > 0) {
+                data.splice(data.begin(), local_front[i]);
+            }
+            if (local_back[i].size() > 0) {
+                data.splice(data.end(), local_back[i]);
+            }
+        }
+    }
+
+    // [todo] fix as nested template
+#define ITERATOR_CLASS(CLASS_NAME, LIST_ITER_TYPE) \
+    class CLASS_NAME    \
+    {                   \
+    public:                                             \
+        CLASS_NAME(typename LIST_ITER_TYPE I, typename LIST_ITER_TYPE E)  \
+        : ITER(I), END_ITER(E) {}                       \
+    inline  bool has_next() {                           \
+        if (ITER == END_ITER) return false;             \
+        else return true;}                              \
+    inline T get_next() {T t = *ITER;ITER++;return t; } \
+    private:                                            \
+        typename LIST_ITER_TYPE ITER;                            \
+        typename LIST_ITER_TYPE END_ITER;                        \
+    };
+
+    ITERATOR_CLASS(seq_iter, std::list<T>::iterator)
+    ITERATOR_CLASS(rev_iter, std::list<T>::reverse_iterator)
+
+#undef ITERATOR_CLASS
+    typedef seq_iter par_iter; // type-alias
+
+    seq_iter prepare_seq_iteration() {
+        seq_iter I(data.begin(), data.end());
+        return I; // copy return
+    }
+//    rev_iter prepare_rev_iteration() {
+//        rev_iter I(data.rbegin(), data.rend());
+//        return I; // copy return
+//    }
+
+    // [xxx] to be implemented
+    par_iter prepare_par_iteration(int thread_id, int max_threads) {
+        assert(false);
+        return NULL;
+    }
+};
+
+typedef gm_seq_vec<node_t> gm_node_seq_vec;
+typedef gm_seq_vec<edge_t> gm_edge_seq_vec;
 
 #endif
